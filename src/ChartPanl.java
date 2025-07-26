51e043732648c5acd665f76b4c659e00b02104a3
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-27 00:53:01 +0200

    Add ChartPanl

---
+++ src/ChartPanl.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.text.StyleConstants.setBackground;
+
+public class ChartPanl extends JPanel{
+    public ChartPanl(){
+        setBackground(Color.LIGHT_GRAY);
+        add(new JLabel("Chart panel"));
+    }
+}
