8ad552bb3a7bb870f6916002e7c680c1a43da0cf
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-27 00:52:32 +0200

    Add CalculatorPanel

---
+++ src/CalculatorPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+class CalculatorPanel extends JPanel {
+    public CalculatorPanel() {
+        setBackground(Color.LIGHT_GRAY);
+        add(new JLabel("Calculator Panel"));
+    }
+}
