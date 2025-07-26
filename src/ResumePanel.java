fcdcb12d2c3a0f498fffd7f853b71e90a3a73de4
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-27 00:53:16 +0200

    Add ResumePanel

---
+++ src/ResumePanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ResumePanel extends JPanel{
+    public ResumePanel(){
+        setBackground(Color.LIGHT_GRAY);
+        add(new JLabel("Resume panel"));
+    }
+}
