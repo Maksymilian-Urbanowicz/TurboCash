ea0414fa7f761b3a3872d37eb0268d0495fe2e0b
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-27 00:53:42 +0200

    Update MainFrame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,7 +6,13 @@ public class MainFrame extends JFrame {
         setTitle("TurboCash – your currency courses");
         setSize(1200, 800);
         setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        add(new TemplatePanel(), BorderLayout.CENTER);
         setVisible(true);
+
     }
 
 }
