21af67520ad03251ee772e5e9706fd049a66be6a
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-27 00:51:18 +0200

    Add FooterPanel

---
+++ src/FooterPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FooterPanel extends JPanel {
+    GridBagConstraints gbc = new GridBagConstraints();
+    public FooterPanel() {
+        setLayout(new GridBagLayout());
+        gbc.gridx=0;
+        gbc.gridy=0;
+        gbc.insets = new Insets(5,0,5,0);
+        add(new JLabel("Maksymilian Urbanowicz \u00A9 2025 TurboCash"), gbc);
+    }
+}
+
