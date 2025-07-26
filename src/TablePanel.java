5f5a1fd1a810f1ce6c6f2f205301c67aa553ae29
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-27 00:53:25 +0200

    Add TablePanel

---
+++ src/TablePanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TablePanel extends JPanel {
+    public TablePanel(){
+        setBackground(Color.LIGHT_GRAY);
+        add(new JLabel("Table panel"));
+    }
+}
