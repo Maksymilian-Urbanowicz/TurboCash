2ba42366da92dce91529f562118934285cbc23cb
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-26 22:09:56 +0200

    Create initial project structure

---
+++ src/MainFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        setTitle("TurboCash – your currency courses");
+        setSize(1200, 800);
+        setResizable(false);
+        setVisible(true);
+    }
+
+}
