e75571df53a1481ac8611cb8ff8c0a77bea5e6ac
Maksymilian Urbanowicz <maksurbanowicz@gmail.com>
2025-07-28 00:05:50 +0200

    Add method getCurrency in NBPAPI to get the currency course

--- src/Activity/API/NBPAPI.java
+++ src/Activity/API/NBPAPI.java
@@ -1,7 +1,9 @@
 package Activity.API;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class NBPAPI {
     private HttpClient client;
@@ -9,5 +11,10 @@ public class NBPAPI {
     public NBPAPI(){
         this.client = HttpClient.newHttpClient();
         this.request = (HttpRequest) HttpRequest.newBuilder();
+    }
+
+    public HttpResponse<String> getCurrency(String currency) throws IOException, InterruptedException {
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response;
     }
 }
