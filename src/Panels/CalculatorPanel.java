56841f0f20b3abe9fa8f37714064179a205d0d9f
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-07-29 16:33:49 +0200

    Update constructor in CalculatorPanel class

--- src/Panels/CalculatorPanel.java
+++ src/Panels/CalculatorPanel.java
@@ -1,11 +1,19 @@
 package Panels;
 
+import Activity.Calculator;
+import Models.CurrencyData;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class CalculatorPanel extends JPanel {
-    public CalculatorPanel() {
+    private Calculator calculator;
+    public CalculatorPanel() throws IOException, URISyntaxException, InterruptedException {
         setBackground(Color.LIGHT_GRAY);
-        add(new JLabel("Calculator Panel"));
+        calculator = new Calculator();
+        CurrencyData currency = calculator.getCurrencyInfo("usd");
+        add(new JLabel("Calculator Panel\n " + currency.getName() + currency.getAsk()));
     }
 }
