3b6559ce6a355e17c0c9cda2ad3ad83df2aac5ce
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-07-29 16:34:43 +0200

    Add getCurrencyInfo method in Calculator class to communicate with getCurrency method in NBPAPI class

--- src/Activity/Calculator.java
+++ src/Activity/Calculator.java
@@ -1,4 +1,33 @@
 package Activity;
 
+import Activity.API.NBPAPI;
+import Models.CurrencyData;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Currency;
+import java.util.Map;
+
 public class Calculator {
+    private NBPAPI nbp;
+
+    public Calculator() {
+        nbp = new NBPAPI();
+    }
+
+    public CurrencyData getCurrencyInfo(String code) throws IOException, URISyntaxException, InterruptedException {
+        JSONObject data = nbp.getCurrency(code);
+        CurrencyData currency = new CurrencyData(
+                data.getString("currency"),
+                data.getString("code"),
+                data.getJSONArray("rates").getJSONObject(0).getDouble("bid"),
+                data.getJSONArray("rates").getJSONObject(0).getDouble("ask")
+        );
+
+        return currency;
+    }
+
+
 }
