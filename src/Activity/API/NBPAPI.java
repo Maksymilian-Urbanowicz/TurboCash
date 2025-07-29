7444d30bc79e95a2f1b6cff33e959b68366dd8f0
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-07-29 16:31:36 +0200

    Update getCurrency method in NBPAPI class

--- src/Activity/API/NBPAPI.java
+++ src/Activity/API/NBPAPI.java
@@ -1,27 +1,35 @@
 package Activity.API;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class NBPAPI {
     private HttpClient client;
-    private HttpRequest request;
+
     public NBPAPI(){
         this.client = HttpClient.newHttpClient();
-        this.request = (HttpRequest) HttpRequest.newBuilder();
     }
 
-    public HttpResponse<String> getCurrency(String currency) throws IOException, InterruptedException {
+    public JSONObject getCurrency(String code) throws IOException, InterruptedException, URISyntaxException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://api.nbp.pl/api/exchangerates/rates/c/"+code+"/today/?format=json"))
+                .GET()
+                .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response;
+        return new JSONObject(response.body());
     }
 
-    public HttpResponse<String> getTable(String tableKind) throws IOException, InterruptedException {
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response;
-    }
+//    public HttpResponse<String> getTable(String tableKind) throws IOException, InterruptedException {
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        return response;
+//    }
+
 
 
 }
