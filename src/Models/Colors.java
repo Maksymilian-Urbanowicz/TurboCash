fcb9231f5b2ff572c4638d198d8c666dccfd81f5
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-07-30 23:09:43 +0200

    Update colors in Colors class

--- src/Models/Colors.java
+++ src/Models/Colors.java
@@ -3,5 +3,10 @@ package Models;
 import java.awt.*;
 
 public class Colors {
-    public static final Color BACKGROUND_COLOR = new Color(46, 46, 46);
+    public static final Color BACKGROUND = new Color(230, 230, 230);
+    public static final Color TEXT = new Color(0, 0, 0);
+    public static final Color TITLE = new Color(37, 34, 34);
+    public static final Color BUTTON_BACKGROUND = new Color(0, 120, 215);
+    public static final Color BUTTON_FONT = new Color(230, 230, 230);
+    public static final Color ERROR = new Color(255, 0, 0);
 }
