1edf0cfb7ae96e3db3698ea7934d8046087806fa
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-07-30 23:09:18 +0200

    Add Fonts class

---
+++ src/Models/Fonts.java
@@ -0,0 +1,8 @@
+package Models;
+
+import java.awt.*;
+
+public class Fonts {
+    public static final Font TITLE_FONT = new Font("Segoe UI", Font.BOLD, 18);
+    public static final Font LABEL_FONT = new Font("Segoe UI", Font.PLAIN, 16);
+}
