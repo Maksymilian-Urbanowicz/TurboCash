7700e950d976f87b0b88c28d562b729f75078b33
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-08-04 21:10:01 +0200

    Add table colors in Colors class

--- src/Models/Colors.java
+++ src/Models/Colors.java
@@ -9,4 +9,7 @@ public class Colors {
     public static final Color BUTTON_BACKGROUND = new Color(0, 120, 215);
     public static final Color BUTTON_FONT = new Color(230, 230, 230);
     public static final Color ERROR = new Color(255, 0, 0);
+    public static final Color TABLE_ROW_ODD = new Color(255, 255, 255);
+    public static final Color TABLE_ROW_EVEN = new Color(230, 230, 255);
+    public static final Color TABLE_HEADER = new Color(255, 255, 255);
 }
