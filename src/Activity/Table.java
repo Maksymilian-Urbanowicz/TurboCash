697c5c03dd951c5451b5db39fb0a506839f451f5
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-08-04 18:17:21 +0200

    Add getDataTable method in Table class to transfer data between API and TablePanel class.

--- src/Activity/Table.java
+++ src/Activity/Table.java
@@ -1,4 +1,37 @@
 package Activity;
 
+import Activity.API.CurrencyNameTranslatorAPI;
+import Activity.API.NBPAPI;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Table {
+    private NBPAPI nbp;
+    private CurrencyNameTranslatorAPI translatorAPI;
+
+    public Table() {
+        nbp = new NBPAPI();
+        translatorAPI = new CurrencyNameTranslatorAPI();
+    }
+
+    public String[][] getDataTable() throws IOException, URISyntaxException, InterruptedException {
+        JSONArray jsonArray = nbp.getTableActual('a');
+        JSONObject data = jsonArray.getJSONObject(0);
+        JSONArray rates = data.getJSONArray("rates");
+
+        String[][] cols = new String[rates.length()][3];
+        for (int i = 0; i < rates.length(); i++) {
+            JSONObject rate = rates.getJSONObject(i);
+            cols[i] = new String[]{
+                    rate.getString("code"),
+                    translatorAPI.translate(rate.getString("code")),
+                    String.valueOf(rate.getDouble("mid"))
+            };
+        }
+
+        return cols;
+    }
 }
