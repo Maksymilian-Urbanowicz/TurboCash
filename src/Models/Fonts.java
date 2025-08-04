6d5145bb050ef54935e2c1fbbabe496ccb38cafd
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-08-04 21:10:13 +0200

    Add table fonts in Fonts class

--- src/Models/Fonts.java
+++ src/Models/Fonts.java
@@ -5,4 +5,6 @@ import java.awt.*;
 public class Fonts {
     public static final Font TITLE_FONT = new Font("Segoe UI", Font.BOLD, 18);
     public static final Font LABEL_FONT = new Font("Segoe UI", Font.PLAIN, 16);
+    public static final Font TABLE_HEADER_FONT = new Font("Segoe UI", Font.BOLD, 14);
+    public static final Font TABLE_CEIL_FONT = new Font("Segoe UI", Font.PLAIN, 12);
 }
