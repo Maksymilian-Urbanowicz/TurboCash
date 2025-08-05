94c823d2e746bf0cc9b819fd477328e67064bfb5
Maksymilian Urbanowicz <80683009+Maksymilian-Urbanowicz@users.noreply.github.com>
2025-08-05 17:09:08 +0200

    Add INPUT_CONTROLS, BUTTON fonts to Fonts class. Update names of variable

--- src/Models/Fonts.java
+++ src/Models/Fonts.java
@@ -3,8 +3,10 @@ package Models;
 import java.awt.*;
 
 public class Fonts {
-    public static final Font TITLE_FONT = new Font("Segoe UI", Font.BOLD, 18);
-    public static final Font LABEL_FONT = new Font("Segoe UI", Font.PLAIN, 16);
-    public static final Font TABLE_HEADER_FONT = new Font("Segoe UI", Font.BOLD, 14);
-    public static final Font TABLE_CEIL_FONT = new Font("Segoe UI", Font.PLAIN, 12);
+    public static final Font TITLE = new Font("Segoe UI", Font.BOLD, 18);
+    public static final Font LABEL = new Font("Segoe UI", Font.PLAIN, 16);
+    public static final Font BUTTON= new Font("Segoe UI", Font.PLAIN, 16);
+    public static final Font INPUT_CONTROLS = new Font("Segoe UI", Font.PLAIN, 16);
+    public static final Font TABLE_HEADER = new Font("Segoe UI", Font.BOLD, 14);
+    public static final Font TABLE_CEIL = new Font("Segoe UI", Font.PLAIN, 12);
 }
